33837e66b8417e4817c1d58b1700aea2368d6d93
amateur-dev <me@dipeshsukhani.dev>
2022-07-09 07:08:23 +0800

    fix(smartcontracts): split up the smart contracts and added more checks and reorg some gitignore

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Unlicensed
 
-pragma solidity >0.7.0 <=0.9.0;
-
+pragma solidity >0.8.0 <=0.9.0;
 
 contract Campaign {
     string public title;
@@ -10,9 +9,10 @@ contract Campaign {
     string public story;
     address payable public owner;
     uint public receivedAmount;
+    bool public acceptingDonation = true;
 
     event donated(address indexed donar, uint indexed amount, uint indexed timestamp);
-    event withdrawn(address campaignowner, uint indexed amount);
+    event withdrawn(uint amount);
 
     constructor(
         string memory campaignTitle, 
@@ -27,60 +27,23 @@ contract Campaign {
         story = storyURI;
         owner = payable(campaignOwner);
     }
-   
 
     function donate() public payable {
+        require(msg.value>0,"Msg.value = 0");
+        require(msg.value <= requiredAmount - receivedAmount, "Campaign does not required these many funds");
         receivedAmount += msg.value;
+        if (receivedAmount == requiredAmount) {
+            acceptingDonation = false;
+        }
         emit donated(msg.sender, msg.value, block.timestamp);
     }
     function withdraw() public payable {
+        require(!acceptingDonation, "Campaign still not over");
         require(msg.sender == owner, "Only the owner of the campaign can withdraw");
-        require(receivedAmount > requiredAmount, "Required amount not fulfilled yet");
         owner.transfer(receivedAmount);
-        receivedAmount = 0;
-    emit withdrawn(msg.sender, msg.value);
-    }
-
-    
-}
-
-contract CampaignFactory {
-    address[] public deployedCampaigns;
-
-    event campaignCreated(
-        string title,
-        uint requiredAmount,
-        address indexed owner,
-        address campaignAddress,
-        string imgURI,
-        uint indexed timestamp,
-        string indexed category
-    );
-
-    function createCampaign(
-        string memory campaignTitle, 
-        uint requiredCampaignAmount, 
-        string memory imgURI, 
-        string memory category,
-        string memory storyURI) public
-    {
-
-        Campaign newCampaign = new Campaign(
-            campaignTitle, requiredCampaignAmount, imgURI, storyURI, msg.sender);
-        
-
-        deployedCampaigns.push(address(newCampaign));
-
-        emit campaignCreated(
-            campaignTitle, 
-            requiredCampaignAmount, 
-            msg.sender, 
-            address(newCampaign),
-            imgURI,
-            block.timestamp,
-            category
-        );
-
+        emit withdrawn(receivedAmount);
     }
 }
+
+
 
