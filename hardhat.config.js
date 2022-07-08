03960b60a2cabc66ac5a66d124498d97d7ebb9d3
paritomarrr <pari2019cse083abesit@gmail.com>
2022-07-08 23:39:39 +0530

    UI fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,8 +18,9 @@ module.exports = {
     hardhat: {},
     buildbear: {
       url: "https://backend.buildbear.io/node/bold-bohr-2aa906",
-      accounts: ['3d2336ebefb8b312bb716771f9c4c6d41ff70102b3a77b2efb5a80a408d3ef29']
+      accounts: [privateKey]
     }
   }
 };
 
+// chain id 8301
