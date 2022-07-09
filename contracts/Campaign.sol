0a305816f4afcd7197211da56ffec71a7cba3914
amateur-dev <me@dipeshsukhani.dev>
2022-07-09 11:13:05 +0800

    WIP

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -13,6 +13,8 @@ contract Campaign {
 
     event donated(address indexed donar, uint indexed amount, uint indexed timestamp);
     event withdrawn(uint amount);
+    
+    error TooHighDonation(string message, uint256 donationRemaining);
 
     constructor(
         string memory campaignTitle, 
@@ -28,21 +30,36 @@ contract Campaign {
         owner = payable(campaignOwner);
     }
 
+
     function donate() public payable {
-        require(msg.value>0,"Msg.value = 0");
-        require(msg.value <= requiredAmount - receivedAmount, "Campaign does not required these many funds");
+        require(msg.value > 0,"Msg.value = 0");
+        if (msg.value > requiredAmount - receivedAmount) revert TooHighDonation({
+                message: "Donation room left",
+                donationRemaining:requiredAmount - receivedAmount
+            });
         receivedAmount += msg.value;
-        if (receivedAmount == requiredAmount) {
+        if (receivedAmount >= requiredAmount) {
             acceptingDonation = false;
         }
         emit donated(msg.sender, msg.value, block.timestamp);
     }
+
     function withdraw() public payable {
         require(!acceptingDonation, "Campaign still not over");
         require(msg.sender == owner, "Only the owner of the campaign can withdraw");
         owner.transfer(receivedAmount);
         emit withdrawn(receivedAmount);
     }
+
+    //@TODO: need to check why is the contract not receiving direct messages
+    fallback() external payable {
+        donate();
+    }
+
+    receive() external payable {
+        donate();
+    }
+
 }
 
 
