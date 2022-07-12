eaf6121f7f3d56425759fa8ae9698792cba5bfe4
paritomarrr <pari2019cse083abesit@gmail.com>
2022-07-12 18:52:25 +0530

    bug fix

--- pages/createcampaign.js
+++ pages/createcampaign.js
@@ -1,17 +1,13 @@
-import { useContext } from 'react';
 import Form from '../components/Form/Form';
-import AdminContext from '../context/adminContext';
 
 
-
-
-const Createcampaign = () => {
-  const { newSigner, setNewSigner, newAddress, setNewAddress, allCampaigns, setAllCampaigns, connectAccount, setConnectAccount,  fetchChainId, setFetchChainId  } = useContext(AdminContext)
+const createcampaign = () => {
+ 
   return (
     <>
-    {(connectAccount && connectAccount == 8337) ?<Form /> : <p style={{fontFamily: 'Poppins', textAlign: 'center', paddingTop: '50px'}}>Please make sure that you are connected to wallet</p>}
+   <Form />
     </>
   )
 }
 
-export default Createcampaign
+export default createcampaign
