f0ab65ea4232641891b956506f5e580a82acfd66
paritomarrr <pari2019cse083abesit@gmail.com>
2022-07-12 09:54:04 +0530

    bug fix

--- context/adminContext.js
+++ context/adminContext.js
@@ -4,9 +4,9 @@ const AdminContext = createContext();
 
 export const SignerProvider = (props) => {
   //this will be the Admin object that will store all data
-  const [newSigner, setNewSigner] = useState();
+  const [newSigner, setNewSigner] = useState({});
   const [newAddress, setNewAddress] = useState('');
-  const [allCampaigns, setAllCampaigns] = useState();
+  const [allCampaigns, setAllCampaigns] = useState([]);
   const [connectAccount, setConnectAccount] = useState(false)
 
   return (
