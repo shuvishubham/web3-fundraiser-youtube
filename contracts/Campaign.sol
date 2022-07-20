53453856b4b264dc84a01115d9af69497d7187ac
paritomarrr <pari2019cse083@gmail.com>
2022-07-20 09:45:30 +0530

    contract updated

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,48 +3,50 @@
 pragma solidity >0.8.0 <=0.9.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
 contract Campaign is ReentrancyGuard {
-
-    mapping( address => uint ) public contributers;
+    mapping(address => uint256) public contributers;
     string public title;
-    uint public requiredAmount;
+    uint256 public requiredAmount;
     string public image;
     string public story;
     address payable public owner;
-    uint public receivedAmount;
+    uint256 public receivedAmount;
     bool public acceptingDonation = true;
-    uint public deadline;
+    uint256 public deadline;
 
-    event donated(address indexed donar, uint indexed amount, uint indexed timestamp);
-    event withdrawn(uint amount);
-    
+    event donated(
+        address indexed donar,
+        uint256 indexed amount,
+        uint256 indexed timestamp
+    );
+    event withdrawn(uint256 amount);
+
     error TooHighDonation(string message, uint256 donationRemaining);
 
     constructor(
-        string memory campaignTitle, 
-        uint requiredCampaignAmount, 
+        string memory campaignTitle,
+        uint256 requiredCampaignAmount,
         string memory imgURI,
         string memory storyURI,
         address campaignOwner,
-        uint campaignDeadline
-    ) ReentrancyGuard () {
+        uint256 campaignDeadline
+    ) ReentrancyGuard() {
         title = campaignTitle;
         requiredAmount = requiredCampaignAmount;
         image = imgURI;
         story = storyURI;
         owner = payable(campaignOwner);
-        deadline = campaignDeadline; 
+        deadline = campaignDeadline;
     }
-
 
     function donate() public payable {
         require(acceptingDonation);
         require(block.timestamp < deadline, "Deadline has Ended!");
-        require(msg.value > 0,"Msg.value = 0");
-        if (msg.value > requiredAmount - receivedAmount) revert TooHighDonation({
+        require(msg.value > 0, "Msg.value = 0");
+        if (msg.value > requiredAmount - receivedAmount)
+            revert TooHighDonation({
                 message: "Donation room left",
-                donationRemaining:requiredAmount - receivedAmount
+                donationRemaining: requiredAmount - receivedAmount
             });
         receivedAmount += msg.value;
         contributers[msg.sender] += msg.value;
@@ -52,23 +54,24 @@ contract Campaign is ReentrancyGuard {
             acceptingDonation = false;
         }
         emit donated(msg.sender, msg.value, block.timestamp);
-          
     }
 
     function withdraw() public {
         require(!acceptingDonation, "Campaign still not over");
-        require(msg.sender == owner, "Only the owner of the campaign can withdraw");
+        require(
+            msg.sender == owner,
+            "Only the owner of the campaign can withdraw"
+        );
         owner.transfer(receivedAmount);
         emit withdrawn(receivedAmount);
     }
 
-
-    function refund () public {
+    function refund() public {
         require(block.timestamp > deadline && receivedAmount < requiredAmount);
-        require(contributers[msg.sender] > 0,"Not eligible for refund");
+        require(contributers[msg.sender] > 0, "Not eligible for refund");
         address payable user = payable(msg.sender);
-        user.transfer(contributers[msg.sender]);
         contributers[msg.sender] = 0;
+        user.transfer(contributers[msg.sender]);
     }
 
     fallback() external payable {
@@ -78,5 +81,4 @@ contract Campaign is ReentrancyGuard {
     receive() external payable {
         donate();
     }
-
 }
